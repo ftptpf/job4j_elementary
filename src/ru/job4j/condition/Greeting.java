d156474a8ee44195a7c6c0d1698a143c5552c3f9
ftptpf <Nb49Rtdd>
2020-05-05 20:57:36 +0300

    9.1. String [#283763]

---
+++ src/ru/job4j/condition/Greeting.java
@@ -0,0 +1,11 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String [] args){
+        String idea = "I love Java!";
+        System.out.println(idea + " But I am newbie.");
+        int year = 2020;
+        String separate = " ";
+        System.out.println(idea + separate + year);
+    }
+}
