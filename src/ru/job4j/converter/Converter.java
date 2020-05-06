a3816c4df975738969db643bfa26c0da23067444
ftptpf <Nb49Rtdd>
2020-05-06 09:31:26 +0300

    9.1. String [#283763]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -1,18 +1,28 @@
 package ru.job4j.converter;
 
 public class Converter {
-    public static float rubleToEuro(float value){
+    public static float rubleToEuro(float value) {
         float rls = value / 70;
         return rls;
     }
-    public static float rubleToDollar (float value){
+    public static float rubleToDollar(float value) {
         float rls = value / 60;
         return  rls;
     }
-    public static void main (String[] args){
-        float euro = Converter.rubleToEuro(140);
-        float dollar = Converter.rubleToDollar(140);
+    public static void main(String[] args) {
+        double euro = Converter.rubleToEuro(140);
+        double dollar = Converter.rubleToDollar(140);
         System.out.println("140 ruble are " + euro + " euros. ");
         System.out.println("140 ruble are " + dollar + " dollars. ");
+        //test Euro result
+        float in = 140;
+        double expected = 2.0;
+        float out = rubleToEuro(in);
+        boolean passed = expected == out;
+        System.out.println("140 ruble are 2.0 euro. Test result: " + passed);
+        //test Dollar result
+        expected = 2.3333333;
+        out = rubleToDollar(in);
+        System.out.println("140 ruble are 2.3333333 dollar. Test result: " + passed);
     }
 }
