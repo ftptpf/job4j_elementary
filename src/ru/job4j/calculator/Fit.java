6e0b3e9f75169d3eafcf85bd225ea20910949933
ftptpf <Nb49Rtdd>
2020-05-08 20:42:01 +0300

    6. Идеальный вес. [#283753]

--- src/ru/job4j/calculator/Fit.java
+++ src/ru/job4j/calculator/Fit.java
@@ -1,15 +1,15 @@
 package ru.job4j.calculator;
 
 public class Fit {
-    public static double manWeight (double height){
-        double rls = (height - 100)*1.15;
+    public static double manWeight(double height) {
+        double rls = (height - 100) * 1.15;
         return rls;
     }
-    public static double womanWeight (double height){
-        double rls = (height - 110)*1.15;
+    public static double womanWeight(double height) {
+        double rls = (height - 110) * 1.15;
         return rls;
     }
-    public static void main(String [] args){
+    public static void main(String[] args) {
         double man = Fit.manWeight(180);
         double woman = Fit.womanWeight(170);
         System.out.println("The ideal man weight for height 180 centimeters is " + man + " kg.");
