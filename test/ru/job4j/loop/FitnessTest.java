a60bca7d6e88612db95bccd4e77b0d84480d1be1
ftptpf <Nb49Rtdd>
2020-05-15 20:11:50 +0300

    5.4. Протеиновая диета. [#283802] - 90-90

--- test/ru/job4j/loop/FitnessTest.java
+++ test/ru/job4j/loop/FitnessTest.java
@@ -8,18 +8,30 @@ import static org.hamcrest.Matchers.is;
 public class FitnessTest {
 
     @Test
-    public void whenGreatNik() {
+    //Показатели Ивана лучше чем у Никиты
+    public void whenIvanGreatNik() {
         int month = Fitness.calc(95, 90);
         assertThat(month, is(0));
     }
+
     @Test
+    //Показатели Ивана хуже чем у Никиты
     public void whenIvanLessNik() {
         int month = Fitness.calc(90, 95);
         assertThat(month, is(1));
     }
+
     @Test
+    //Показатели Ивана намного хуже чем у Никиты
     public void whenIvanLessByFewNik() {
         int month = Fitness.calc(50, 95);
+        assertThat(month, is(2));
+    }
+
+    @Test
+    //Показатели Ивана такие же как у Никиты
+    public void whenIvanTheSameNik() {
+        int month = Fitness.calc(90, 90);
         assertThat(month, is(1));
     }
 }
