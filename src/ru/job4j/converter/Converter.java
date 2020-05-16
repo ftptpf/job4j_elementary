e38fb30e7ec5537d3d36dcc13f664bc046b92872
ftptpf <Nb49Rtdd>
2020-05-16 17:48:55 +0300

    5.8. JavaDoc. Code convention [#283813]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -1,14 +1,36 @@
 package ru.job4j.converter;
 
+/**
+* Class Converter конвертер валют Ruble-Dollar, Ruble-Euro
+* @author Andrey
+ * @since 16.05.2020
+ * @version 1.0.0
+*/
 public class Converter {
+    /**
+    * Метод конвертации рублей в евро
+     * @param value значение в рублях
+     * @return rls возращает значение в евро
+    */
     public static float rubleToEuro(float value) {
         float rls = value / 70;
         return rls;
     }
+
+    /**
+     * Метод конвертации рублей в доллары
+     * @param value значение в рублях
+     * @return rls возращает значение в доларах
+     */
     public static float rubleToDollar(float value) {
         float rls = value / 60;
         return  rls;
     }
+
+    /**
+     * Главный метод класса
+     * @param args - args массив строковых параметров метода
+     */
     public static void main(String[] args) {
         double euro = Converter.rubleToEuro(140);
         double dollar = Converter.rubleToDollar(140);
