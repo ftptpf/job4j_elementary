ccf3585e37729ec071755f474e54c541f6ace096
ftptpf <Nb49Rtdd>
2020-05-16 16:24:13 +0300

    5.6. Простые числа. [#283803]

---
+++ src/ru/job4j/loop/PrimeNumber.java
@@ -0,0 +1,14 @@
+package ru.job4j.loop;
+
+public class PrimeNumber {
+    public int calc(int finish) {
+        int count = 0;
+        for (int n = 2;  n <= finish; n++) {
+            if (CheckPrimeNumber.check(n)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+}
