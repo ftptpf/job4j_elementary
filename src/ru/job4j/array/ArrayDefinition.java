5d867d8e8c8a430a5c76e0750c940203b26a7d57
ftptpf <Nb49Rtdd>
2020-05-17 11:03:19 +0300

    6.0.2. Заполнение массива. [#283741]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -5,5 +5,16 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surname = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+
+        names[0] = "Иван";
+        names[1] = "Семён";
+        names[2] = "Григорий";
+        names[3] = "Николай";
+
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
