74ca804c25005ee9d1a1ae93294ddc88480c76c4
ftptpf <Nb49Rtdd>
2020-05-20 20:43:50 +0300

    6.6.1. Двухмерный массив. Вывод на консоль. [#283747]

---
+++ src/ru/job4j/array/BarleyBreak.java
@@ -0,0 +1,16 @@
+package ru.job4j.array;
+
+public class BarleyBreak {
+    public static void main(String[] args) {
+        int[][] mas = new int[3][3];
+        mas[0][0] = 1;
+        mas[0][1] = 2;
+        mas[0][2] = 3;
+        mas[1][0] = 4;
+        mas[1][1] = 5;
+        mas[1][2] = 6;
+        mas[2][0] = 7;
+        mas[2][1] = 8;
+        mas[2][2] = 9;
+    }
+}
