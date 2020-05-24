a6913cd1bf6779c8a8b907f00d48567905634834
ftptpf <Nb49Rtdd>
2020-05-24 14:39:12 +0300

    6.7.4. Выигрышные комбинации в сокобан [#283731] -1

--- src/ru/job4j/array/MatrixCheck.java
+++ src/ru/job4j/array/MatrixCheck.java
@@ -3,7 +3,7 @@ package ru.job4j.array;
 public class MatrixCheck {
     public static boolean monoHorizontal(char[][] board, int row) {
         boolean result = true;
-        for (int i = 0; i < board.length - 1; i++) {
+        for (int i = 0; i < board.length; i++) {
             if (board[row][i] != 'X') {
                 result = false;
                 break;
@@ -13,7 +13,7 @@ public class MatrixCheck {
     }
     public static boolean monoVertical(char[][] board, int column) {
         boolean result = true;
-        for (int i = 0; i < board.length - 1; i++) {
+        for (int i = 0; i < board.length ; i++) {
             if (board[i][column] != 'X') {
                 result = false;
                 break;
@@ -30,7 +30,7 @@ public class MatrixCheck {
     }
     public static boolean isWin(char[][] board) {
         boolean result = false;
-        for (int index = 0; index < board.length - 1; index++) {
+        for (int index = 0; index < board.length; index++) {
             if (monoHorizontal(board, index) || monoVertical(board, index)) {
                 result = true;
                 break;
